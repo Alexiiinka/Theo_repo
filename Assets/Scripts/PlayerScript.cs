7295066144d25af157f65783c929f758e57b8271
Alexiiinka <sasenka98@gmail.com>
2022-07-07 17:56:36 +0200

    Player script
    -basic player movement
    -trigger with fish
    + added colliders2D

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 move;
+    public float horizontalInput, verticalInput;
+    [SerializeField]
+    private int speed = 3;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        move = new Vector2(horizontalInput, verticalInput) * speed;
+        rb.velocity = move;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Fishes"))
+        {
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
